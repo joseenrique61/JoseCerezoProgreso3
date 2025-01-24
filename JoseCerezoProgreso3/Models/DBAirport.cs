1d3b25bc5879e72bac8dcd49c6656eb1667b183f
José Enrique Cerezo <joseenrique61@hotmail.com>
2025-01-24 07:31:11 -0500

    Modelos añadidos

---
+++ JoseCerezoProgreso3/Models/DBAirport.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+
+namespace JoseCerezoProgreso3.Models
+{
+	public class DBAirport
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Country { get; set; }
+		
+		public float Longitude { get; set; }
+
+		public float Latitude { get; set; }
+
+		public string Email { get; set; }
+
+		public string MyName => "JCerezo";
+	}
+}
---
+++ JoseCerezoProgreso3/Models/JSONAirport.cs
@@ -0,0 +1,45 @@
+﻿namespace JoseCerezoProgreso3.Models
+{
+	public class JSONAirport
+	{
+		public class Rootobject
+		{
+			public int id { get; set; }
+			public string name { get; set; }
+			public string code { get; set; }
+			public Location location { get; set; }
+			public string city { get; set; }
+			public string country { get; set; }
+			public string timezone { get; set; }
+			public Terminal[] terminals { get; set; }
+			public string[] airlines { get; set; }
+			public string[] services { get; set; }
+			public Contact_Info contact_info { get; set; }
+		}
+
+		public class Location
+		{
+			public float latitude { get; set; }
+			public float longitude { get; set; }
+		}
+
+		public class Contact_Info
+		{
+			public string phone { get; set; }
+			public string email { get; set; }
+			public string website { get; set; }
+		}
+
+		public class Terminal
+		{
+			public string name { get; set; }
+			public Gate[] gates { get; set; }
+		}
+
+		public class Gate
+		{
+			public string gate_number { get; set; }
+			public string[] airlines { get; set; }
+		}
+	}
+}
