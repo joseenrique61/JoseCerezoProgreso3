cca4fa5e89b92d9d46aa5837ffcebd546f442db9
José Enrique Cerezo <joseenrique61@hotmail.com>
2025-01-24 08:17:43 -0500

    Conexión con la api creada

---
+++ JoseCerezoProgreso3/Utilities/APIClient.cs
@@ -0,0 +1,21 @@
+﻿using JoseCerezoProgreso3.Models;
+using System.Net.Http.Json;
+
+namespace JoseCerezoProgreso3.Utilities
+{
+	public class APIClient : IAPIClient
+	{
+		private readonly HttpClient HttpClient = new();
+
+		public async Task<JSONAirport?> GetAirport(string name)
+		{
+			List<JSONAirport> airports = (await HttpClient.GetFromJsonAsync<List<JSONAirport>>($"https://www.freetestapi.com/api/v1/airports?search={name}&limit=1"))!;
+
+			if (airports.Count == 0)
+			{
+				return null;
+			}
+			return airports[0];
+		}
+	}
+}
---
+++ JoseCerezoProgreso3/Utilities/IAPIClient.cs
@@ -0,0 +1,9 @@
+﻿using JoseCerezoProgreso3.Models;
+
+namespace JoseCerezoProgreso3.Utilities
+{
+	public interface IAPIClient
+	{
+		public Task<JSONAirport?> GetAirport(string name);
+	}
+}
